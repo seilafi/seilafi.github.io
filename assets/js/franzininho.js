08e127c082de996caca436e47fe4606c58d83aeb
Allythy <allythy@protonmail.com>
2019-08-28 14:59:19 -0300

    Menu angoras (#2)
    * Alterações no menu e efeitos de transição do jquery

--- assets/js/franzininho.js
+++ assets/js/franzininho.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
 
   var scrollLink = $('.scroll');
 
-  // Smooth scrolling
-  scrollLink.click(function(e) {
+  var hash = window.location.hash
+  if (hash == '' || hash == '#' || hash == undefined) return false;
+  var target = $(hash);
+  headerHeight = 120;
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+  scrollLink.click(function (e) {
     e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top - 90
-    }, 1000 );
+    if (target.length) {
+      $('html,body').stop().animate({
+        scrollTop: $(this.hash).offset().top - 90 //offsets for fixed header
+      }, 1000);
+    }
   });
 
 })
